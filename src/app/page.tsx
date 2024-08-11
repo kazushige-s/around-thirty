a9ba01ebbbbf6275b81aacf4009ddc61b807cdb6
kazushige-s <74851821+kazushige-s@users.noreply.github.com>
2024-08-11 13:27:50 +0900

    💄 トップページのデザインを変更した。

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
-import { Card, CardSection, Container, Group, Text } from "@mantine/core";
+import {
+  Box,
+  Button,
+  Card,
+  CardSection,
+  Center,
+  Container,
+  Grid,
+  GridCol,
+  Group,
+  Text,
+} from "@mantine/core";
 import React from "react";
 import Image from "next/image";
 import beach from "/src/images/beach.jpg";
-import beach2 from "/src/images/beach2.mp4";
+// import beach2 from "/src/images/beach2.mp4";
 import { Carousel, CarouselSlide } from "@mantine/carousel";
+import CardCopponent from "src/Card/CardCopponent";
 
 export default function Home() {
   return (
@@ -15,21 +27,21 @@ export default function Home() {
 
       {/* Heroエリア */}
       <Container>
-        {/* <Image height={600} width={1600} src={beach} alt="beach" priority /> */}
+        <Image height={600} width={1600} src={beach} alt="beach" priority />
         {/* <video src={beach2} controls playsInline autoPlay loop /> */}
-        <video height={600} width={1600} controls autoPlay loop>
+        {/* <video height={600} width={1600} controls autoPlay loop>
           <source src={beach2} type="video/mp4" />
-        </video>
+        </video> */}
       </Container>
 
       <Container size="100rem">
         {/* pick upエリア */}
-        <h2 className="text-4xl flex justify-center mt-24 mb-7">Pick Up</h2>
+        <h2 className="text-4xl flex justify-center mt-24 mb-7">PICK UP</h2>
 
         {/* カルーセルエリア */}
         <Carousel
           withIndicators
-          height={200}
+          height={370}
           slideSize={200}
           slideGap="md"
           loop
@@ -37,46 +49,82 @@ export default function Home() {
           slidesToScroll={3}
         >
           <CarouselSlide>
-            {/* カードエリア */}
-            <Card shadow="xs" padding="md" radius="lg" withBorder>
-              <CardSection>
-                <Image
-                  height={200}
-                  width={200}
-                  src={beach}
-                  alt="beach"
-                  priority
-                />
-              </CardSection>
-              <Group>
-                <Text fw={500}>White Beach1</Text>
-              </Group>
-              <Text size="sm" c="dimmed">
-                test
-              </Text>
-            </Card>
+            <CardCopponent />
           </CarouselSlide>
           <CarouselSlide>
-            {/* カードエリア */}
-            <Card shadow="xs" padding="md" radius="lg" withBorder>
-              <CardSection>
-                <Image
-                  height={200}
-                  width={200}
-                  src={beach}
-                  alt="beach"
-                  priority
-                />
-              </CardSection>
-              <Group>
-                <Text fw={500}>White Beach1</Text>
-              </Group>
-              <Text size="sm" c="dimmed">
-                test
-              </Text>
-            </Card>
+            <CardCopponent />
+          </CarouselSlide>
+          <CarouselSlide>
+            <CardCopponent />
+          </CarouselSlide>
+          <CarouselSlide>
+            <CardCopponent />
           </CarouselSlide>
         </Carousel>
+
+        {/* コンテンツエリア */}
+        <h2 className="text-4xl flex justify-center mt-24 mb-7">FEATURE</h2>
+        <Grid>
+          <GridCol span={{ base: 12, md: 6, lg: 3 }}>
+            <Center>
+              <CardCopponent />
+            </Center>
+          </GridCol>
+          <GridCol span={{ base: 12, md: 6, lg: 3 }}>
+            <Center>
+              <CardCopponent />
+            </Center>
+          </GridCol>
+          <GridCol span={{ base: 12, md: 6, lg: 3 }}>
+            <Center>
+              <CardCopponent />
+            </Center>
+          </GridCol>
+          <GridCol span={{ base: 12, md: 6, lg: 3 }}>
+            <Center>
+              <CardCopponent />
+            </Center>
+          </GridCol>
+          <GridCol span={{ base: 12, md: 6, lg: 3 }}>
+            <Center>
+              <CardCopponent />
+            </Center>
+          </GridCol>
+          <GridCol span={{ base: 12, md: 6, lg: 3 }}>
+            <Center>
+              <CardCopponent />
+            </Center>
+          </GridCol>
+        </Grid>
+      </Container>
+
+      {/* CONTACTエリア */}
+      <Container size="100rem" className="mt-14">
+        <h2 className="text-4xl flex justify-center mb-7 bg-white">CONTACT</h2>
+        <Grid justify="space-around" align="flex-start" className="mb-12">
+          <GridCol span={{ base: 12, md: 4 }}>
+            <Box>
+              <Text>テキストテキストテキスト</Text>
+              <Text>テキストテキストテキスト</Text>
+              <Text>テキストテキストテキスト</Text>
+            </Box>
+          </GridCol>
+          <GridCol span={{ base: 12, md: 4 }}>
+            <Text size="xl">お問い合わせ</Text>
+            <Box>
+              <Text>名前</Text>
+              <Text>メッセージ</Text>
+              <Button>送信</Button>
+            </Box>
+          </GridCol>
+        </Grid>
+      </Container>
+
+      {/* フッターエリア */}
+      <Container size="100rem" className="mt-14">
+        <Center>
+          <small className="m-2">&copy;White Beach</small>
+        </Center>
       </Container>
     </>
   );
